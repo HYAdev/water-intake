c5f4a574d1003691ac3ee7c18e685d7b0e1fb9bc
Hyena Swan <70421236+HYAdev@users.noreply.github.com>
2023-07-18 20:49:58 -0400

    rwar

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
 # Code, I finished
+
+# Pull, taking the shared code (public) and "downloading" onto your local computer
+
+# push, takes your code, and makes other collaborators able to see this code
+# private -> public
