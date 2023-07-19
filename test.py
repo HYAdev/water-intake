9e7ce41eed68663e63b29ec3d931bf9cfe358897
RobertCFlores <rcflorescd@gmail.com>
2023-07-18 18:04:36 -0700

    asdf

--- test.py
+++ test.py
@@ -1,8 +1,1 @@
-# Code, I finished
-
-# Pull, taking the shared code (public) and "downloading" onto your local computer
-
-# push, takes your code, and makes other collaborators able to see this code
-# private -> public
-
-# ;laksjdf;laksdjfjlasdklfj;alksdjf;lk
+2
